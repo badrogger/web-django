a7f832dd35cd18d26a682db3223f478809f74add
badrogger <badrogger@gmail.com>
2017-07-27 16:20:36 +0300

    mvc task 2 inital

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/jops/learning/web-django/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+
+sudo rm -r /etc/gunicorn.d/*
+sudo ln -sf /home/box/web/etc/hello_gnc.py   /etc/gunicorn.d/hello_gnc.py
+sudo ln -sf /home/box/web/etc/ask_gnc.py   /etc/gunicorn.d/ask_gnc.py
+sudo /etc/init.d/gunicorn restart
+# gunicorn -b 0.0.0.0:8080 hello:app
